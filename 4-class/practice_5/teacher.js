9397b29f3f3264dd22c13074242de3ad7345161d
chentaiyu0336 <2868510447@qq.com>
2020-08-10 22:11:34 +0800

    [cty] finish 4-class

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,16 @@
 // TODO 20: 在这里写实现代码
+import Person from './person';
+
+class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    const classToTeach = this.klass == null ? 'No Class' : `Class ${this.klass}`;
+    return `${super.introduce()} I am a Teacher. I teach ${classToTeach}.`;
+  }
+}
+
+export default Teacher;
