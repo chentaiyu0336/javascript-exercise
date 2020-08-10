ef59dff232f41ca2ec04a21e6bbc5e060149af9c
chentaiyu0336 <2868510447@qq.com>
2020-08-10 20:45:07 +0800

    [cty] finish 3-object

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,7 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let sum = 0;
+  const valuelist = Object.values(source);
+  valuelist.forEach(it => (sum += parseInt(it)));
+  return sum;
 }
